c310f6383ae41cefc4408418fa13dc5819554d1e
hanss314 <hanss314@gmail.com>
2017-09-24 18:34:27 -0700

    Better item searching Quotes are stripped Errors are capitalized

--- percbot/util/converters.py
+++ percbot/util/converters.py
@@ -27,13 +27,13 @@ class UserList(commands.Converter):
 
 class Item(commands.Converter):
     async def convert(self, ctx, argument):
-       
+        
         items = ctx.bot.items
-        argument = argument.lower()
+        argument = argument.lower().strip('"')
         if argument in items: return (argument, items[argument])
         for k, v in items.items():
             if 'aliases' in v and argument in v['aliases']:
                 return (k, v)
             
-        raise commands.BadArgument('{} is not an Item.'.format(argument))
+        raise commands.BadArgument('`{}` is not an Item.'.format(argument.title()))
 
