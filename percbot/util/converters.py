2551620209a051ba67f32942849c6e17097440f1
hanss314 <hanss314@gmail.com>
2017-09-27 18:48:06 -0700

    back to basics

--- percbot/util/converters.py
+++ percbot/util/converters.py
@@ -2,31 +2,27 @@ from discord.ext import commands
 
 class UserList(commands.Converter):
     async def convert(self, ctx, argument):
-        try:
-            ids = set()
-            if ctx.guild is not None:
-                try: member = await commands.MemberConverter().convert(ctx, argument)
-                except commands.BadArgument:
-                    try: role = await commands.RoleConverter().convert(ctx, argument)
-                    except commands.BadArgument as e: raise e
-                    else:
-                        if role.guild.large: await role.bot.request_offline_member(ctx.guild)
-                        for member in role.guild.members:
-                            if role in member.roles:
-                                ids.add(member)
-                else: ids.add(member)
-                    
-            else:
-                try: user = await commands.UserConverter().convert(ctx, argument)
+        ids = set()
+        if ctx.guild is not None:
+            try: member = await commands.MemberConverter().convert(ctx, argument)
+            except commands.BadArgument:
+                try: role = await commands.RoleConverter().convert(ctx, argument)
                 except commands.BadArgument as e: raise e
-                else: ids.add(user)
-            if len(ids) == 0:
-                raise commands.BadArgument('{} is not a role or user.'.format(argument))
-            return ids
-        except Exception as e:
-            await ctx.send(str(e))
-        finally: 
-            await ctx.send('Idk')
+                else:
+                    await ctx.send(str(role))
+                    if role.guild.large: await role.bot.request_offline_member(ctx.guild)
+                    for member in role.guild.members:
+                        if role in member.roles:
+                            ids.add(member)
+            else: ids.add(member)
+                
+        else:
+            try: user = await commands.UserConverter().convert(ctx, argument)
+            except commands.BadArgument as e: raise e
+            else: ids.add(user)
+        if len(ids) == 0:
+            raise commands.BadArgument('{} is not a role or user.'.format(argument))
+        return ids
 
 class Item(commands.Converter):
     async def convert(self, ctx, argument):
