0fc1e4b22a7f62536e907b8f11cdac7fc2b230cb
hanss314 <hanss314@gmail.com>
2017-09-27 18:17:20 -0700

    Forgot to convert to string

--- percbot/util/converters.py
+++ percbot/util/converters.py
@@ -9,7 +9,7 @@ class UserList(commands.Converter):
                 try: role = await commands.RoleConverter().convert(ctx, argument)
                 except commands.BadArgument: pass
                 else:
-                    await ctx.send(role)
+                    await ctx.send(str(role))
                     if role.guild.large: await role.bot.request_offline_member(ctx.guild)
                     for member in role.guild.members:
                         if role in member.roles:
@@ -20,7 +20,7 @@ class UserList(commands.Converter):
             try: user = await commands.UserConverter().convert(ctx, argument)
             except commands.BadArgument: pass
             else: ids.add(user)
-        await ctx.send(ids)
+        await ctx.send(str(ids))
         if len(ids) == 0:
             raise commands.BadArgument('{} is not a role or user.'.format(argument))
         
