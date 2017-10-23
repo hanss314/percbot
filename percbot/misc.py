11124cd0c052761ee8cbf094926502d32974eb26
hanss314 <24215006+hanss314@users.noreply.github.com>
2017-10-22 21:43:26 -0700

    Better help messages

--- percbot/misc.py
+++ percbot/misc.py
@@ -6,39 +6,82 @@ from percbot.util.categories import category
 
 
 class Misc():
+    def __init__(self, bot):
+        self.bot = bot
+
+    def format_args(self, cmd):
+        params = list(cmd.clean_params.items())
+        p_str = ''
+        for p in params:
+            print(p[1], p[1].default, p[1].empty)
+            if p[1].default == p[1].empty:
+                p_str += f' <{p[0]}>'
+            else:
+                p_str += f' [{p[0]}]'
+
+        return p_str
+
+    def format_commands(self, prefix, cmd, name=None):
+        cmd_args = self.format_args(cmd)
+        if not name: name = cmd.name
+        name = name.replace('  ',' ')
+        d = f'`{prefix}{name}{cmd_args}`\n'
+
+        if type(cmd) == commands.core.Group:
+            cmds = sorted(list(cmd.commands), key=lambda x: x.name)
+            for subcmd in cmds:
+                d += self.format_commands(prefix, subcmd, name=f'{name} {subcmd.name}')
+
+        return d
+
+    def get_help(self, ctx, cmd, name=None):
+        d = f'Help for command `{cmd.name}`:\n'
+        d += '\n**Usage:**\n'
+
+        d += self.format_commands(ctx.prefix, cmd, name=name)
+
+        d += '\n**Description:**\n'
+        d += '{}\n'.format('None' if cmd.help is None else cmd.help.strip())
+
+        if cmd.checks:
+            d += '\n**Checks:**'
+            for check in cmd.checks:
+                d += '\n{}'.format(check.__qualname__.split('.')[0])
+            d += '\n'
+
+        if cmd.aliases:
+            d += '\n**Aliases:**'
+            for alias in cmd.aliases:
+                d += f'\n`{ctx.prefix}{alias}`'
+
+            d += '\n'
+
+        return d
+
     @category('info')
     @commands.command()
     async def help(self, ctx, *args):
-        '''This help message :D'''
-        cmds = {i for i in ctx.bot.all_commands.values()}
-
+        '''This help message'''
         if len(args) == 0:
-            d = ''#'**PercBot help:**'
+            cats = [cog for cog in self.bot.cogs]
+            cats.sort()
+            width = max([len(cat) for cat in cats]) + 2
+            d = '**Categories:**\n'
+            for cat in zip(cats[0::2], cats[1::2]):
+                d += '**`{}`**{}**`{}`**\n'.format(cat[0],' ' * int(2.3 * (width-len(cat[0]))), cat[1])
+            if len(cats)%2 == 1:
+                d += '**`{}`**\n'.format(cats[-1])
 
-            cats = {}
-            for cmd in cmds:
-                if not hasattr(cmd, 'category'):
-                    cmd.category = 'Misc'
-                if cmd.category not in cats:
-                    cats[cmd.category] = []
-                cats[cmd.category].append(cmd)
+            d += '\nUse `{0}help <category>` to list commands in a category.\n'.format(ctx.prefix)
+            d += 'Use `{0}help <command>` to get in depth help for a command.\n'.format(ctx.prefix)
 
-            d += '\n**Categories:**\n'
-            for cat in cats:
-                d += '**`{}`**\n'.format(cat)
-            d += '\nUse `{}help <category>` to list commands in a category'.format(ctx.prefix)
-            d += '\nUse `{}help <command>` to get indepth help for a command\n'.format(ctx.prefix)
         elif len(args) == 1:
-            cats = {}
-            for cmd in cmds:
-                if not hasattr(cmd, 'category'):
-                    cmd.category = 'Misc'
-                if cmd.category not in cats:
-                    cats[cmd.category] = []
-                cats[cmd.category].append(cmd)
-            if args[0].title() in cats:
-                d = 'Commands in caterogy **`{}`**:\n'.format(args[0])
-                for cmd in sorted(cats[args[0].title()], key=lambda x:x.name):
+            cats = {cog.lower():cog for cog in self.bot.cogs}
+            if args[0].lower() in cats:
+                cog_name = cats[args[0].lower()]
+                d = 'Commands in category **`{}`**:\n'.format(cog_name)
+                cmds = self.bot.get_cog_commands(cog_name)
+                for cmd in sorted(list(cmds), key=lambda x:x.name):
                     d += '\n  `{}{}`'.format(ctx.prefix, cmd.name)
 
                     brief = cmd.brief
@@ -53,103 +96,31 @@ class Misc():
                     d = 'Command not found.'
                 else:
                     cmd = ctx.bot.all_commands[args[0]]
-                    d = 'Help for command `{}`:\n'.format(cmd.name)
-                    d += '\n**Usage:**\n'
-
-                    if type(cmd) != commands.core.Group:
-                        params = list(cmd.clean_params.items())
-                        p_str = ''
-                        for p in params:
-                            if p[1].default == p[1].empty:
-                                p_str += ' <{}>'.format(p[0])
-                            else:
-                                p_str += ' [{}]'.format(p[0])
-                        d += '`{}{}{}`\n'.format(ctx.prefix, cmd.name, p_str)
-                    else:
-                        d += '`{}{} '.format(ctx.prefix, cmd.name)
-                        if cmd.invoke_without_command:
-                            d += '<'
-                        else:
-                            d += '['
-                        d += '|'.join(cmd.all_commands.keys())
-                        if cmd.invoke_without_command:
-                            d += '>`\n'
-                        else:
-                            d += ']`\n'
-                    
-                    d += '\n**Description:**\n'
-                    d += '{}\n'.format('None' if cmd.help is None else cmd.help.strip())
-
-                    if cmd.checks:
-                        d += '\n**Checks:**'
-                        for check in cmd.checks:
-                            d += '\n{}'.format(check.__qualname__.split('.')[0])
-                        d += '\n'
-
-                    if cmd.aliases:
-                        d += '\n**Aliases:**'
-                        for alias in cmd.aliases:
-                            d += '\n`{}`'.format(alias)
-
-                        d += '\n'
+                    d = self.get_help(ctx, cmd)
         else:
             d = ''
             cmd = ctx.bot
             cmd_name = ''
             for i in args:
                 i = i.replace('@', '@\u200b')
-                if hasattr(cmd, 'all_commands') and i in cmd.all_commands:
+                if cmd == ctx.bot and i in cmd.all_commands:
+                    cmd = cmd.all_commands[i]
+                    cmd_name += cmd.name + ' '
+                elif type(cmd) == commands.Group and i in cmd.all_commands:
                     cmd = cmd.all_commands[i]
                     cmd_name += cmd.name + ' '
                 else:
                     if cmd == ctx.bot:
                         d += 'Command not found.'
                     else:
-                        d += '`{}` has no sub-command `{}`.'.format(cmd.name, i)
+                        d += 'No sub-command found.'.format(cmd.name, i)
                     break
-            if cmd != ctx.bot:
-                d = 'Help for command `{}`:\n'.format(cmd_name)
-                d += '\n**Usage:**\n'
 
-                if type(cmd) != commands.core.Group:
-                    params = list(cmd.clean_params.items())
-                    p_str = ''
-                    for p in params:
-                        if p[1].default == p[1].empty:
-                            p_str += '<{}> '.format(p[0])
-                        else:
-                            p_str += '[{}] '.format(p[0])
-                    d += '`{}{}{}`\n'.format(ctx.prefix, cmd_name, p_str)
-                else:
-                    d += '`{}{} '.format(ctx.prefix, cmd.name)
-                    if cmd.invoke_without_command:
-                        d += '<'
-                    else:
-                        d += '['
-                    d += '|'.join(cmd.all_commands.keys())
-                    if cmd.invoke_without_command:
-                        d += '>`\n'
-                    else:
-                        d += ']`\n'
+            else:
+                d = self.get_help(ctx, cmd, name=cmd_name)
 
-                d += '\n**Description:**\n'
-                d += '{}\n'.format('None' if cmd.help is None else cmd.help.strip())
-
-                if cmd.checks:
-                    d += '\n**Checks:**'
-                    for check in cmd.checks:
-                        d += '\n{}'.format(check.__qualname__.split('.')[0])
-                    d += '\n'
-
-                if cmd.aliases:
-                    d += '\n**Aliases:**'
-                    for alias in cmd.aliases:
-                        d += '\n`{}`'.format(alias)
-
-                    d += '\n'
-
-        d += '\n*Made by hanss314#0128*'
-        await ctx.send(d)
+        # d += '\n*Made by Bottersnike#3605 and hanss314#0128*'
+        return await ctx.send(d)
 
     @category('info')
     @commands.command()
